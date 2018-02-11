749de88fa39d5c41f370867b7a8eef2a91eeb785
cawolf <camilozambranovotto@hotmail.com>
2018-02-10 19:03:03 -0500

    Added bootstrap, navbar and some minor changes :tada:

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+"use strict";
+var x;
+//never run this function, just ignore it's existence
+function algo() {
+  var x = 2, i;
+  for (i = 1; i <= 12; i++) {
+    var y = Math.pow(x, i + 1);
+    x = x * i;
+    x = y - x;
+  }
+  return x;
+}
+function change(something){
+  var check = isNaN(something);
+  if(check){
+    something = 42; //the answer to everything O:
+  }
+  return something;
+}
+x=algo();
+x=change(x);
+x++;       //not 42 anymore :C
