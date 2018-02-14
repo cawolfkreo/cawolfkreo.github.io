75da63033d52830f93036fd2a41df44cc373d7eb
cawolf <camilozambranovotto@hotmail.com>
2018-02-13 23:18:13 -0500

    added bars, more bootstrap and changes in nav
    :tada: Now this looks far more better than before. And it's amazing! I
    still need to change a lot :rage1:

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,5 @@
 "use strict";
-var x, z;
-//never run this function, just ignore it's existence
-function algo() {
-  var x = 2, i;
-  for (i = 1; i <= 12; i++) {
-    var y = Math.pow(x, i + 1);
-    x = x * i;
-    x = y - x;
-  }
-  return x;
-}
-function change(something) {
-  var check = isNaN(something);
-  if (check) {
-    something = 42; //the answer to everything O:
-  }
-  return something;
-}
-x = algo();
-x = change(x);
-x++;       //not 42 anymore
-z = x + 35;
-console.log(z + 3);
+/* global $ */
+$(".navbar-nav>li>a").click( function (){
+  $(".navbar-collapse").collapse("hide"); //collapses the navbar on mobile screens.
+});
