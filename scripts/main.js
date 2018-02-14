fdc8a952f84e0c2ca43e1c6f5c42ae9ca2442e62
cawolf <camilozambranovotto@hotmail.com>
2018-02-14 02:19:07 -0500

    the favicon change dynamically :tada:
    :tada: Now the image can change constantly in the Icon and it's
    supported by chrome. Nice!

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,29 @@
 "use strict";
 /* global $ */
-$(".navbar-nav>li>a").click( function (){
+var frame = 1, elIntervalo;
+console.log(frame+elIntervalo); //sin esto ESlint cree que nunca se usa la variable frame e intervalo.
+console.clear();
+
+var animarFavicon = function () {
+  //Funcion para mantener en animacion constante el favicon de la pagina.
+  var ruta;
+  /* Assignacion del frame a desplegar */
+  window.frame = ++window.frame;
+  if (window.frame > 4) {
+    window.frame = 1; //se asegura de que frame se encuentre entre [1,4].
+  }
+  ruta = "images/frame" + window.frame + ".png";
+  $("#favicon").attr("href", ruta);
+};
+
+var repetirAnimacion = function () {
+  elIntervalo = window.setInterval(animarFavicon, 2000);
+};
+
+$(".navbar-nav>li>a").click(function () {
   $(".navbar-collapse").collapse("hide"); //collapses the navbar on mobile screens.
 });
+
+
+//al momento de estar listo el documento, este se empieza a animar continuamente.
+$(document).ready(repetirAnimacion);
